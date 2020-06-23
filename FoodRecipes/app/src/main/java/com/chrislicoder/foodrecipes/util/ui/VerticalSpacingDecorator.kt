0bcf982ada93dfc7440c2147e605b0e33e97f1d9
Chris Li <chrislicoder@gmail.com>
2020-06-23 22:23:30 +1200

    Have the item decoration

---
+++ FoodRecipes/app/src/main/java/com/chrislicoder/foodrecipes/util/ui/VerticalSpacingDecorator.kt
@@ -0,0 +1,16 @@
+package com.chrislicoder.foodrecipes.util.ui
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class VerticalSpacingDecorator(var verticalSpaceHeight: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.top = verticalSpaceHeight
+    }
+}
